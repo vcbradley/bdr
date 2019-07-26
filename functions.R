ffa64324b69d0f3bcdcd1fb2feaa04fc5935cef2
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-07-26 19:56:41 +0100

    functions and experiments with Pew data

---
+++ functions.R
@@ -0,0 +1,443 @@
+
+calcMSE = function(Y, Y_pred){
+  mean((Y_pred - Y)^2, na.rm = T)
+}
+
+#Utility function for creating a design matrix with all levels of factor variables included, rather than omitting a reference level
+#as one should generally do when passing a design matrix to glmnet/cv.glmnet for a lasso (or elastic net) model
+modmat_all_levs=function(formula, data, sparse = F) UseMethod("modmat_all_levs",data)
+
+modmat_all_levs.data.frame=function(formula, data, sparse = F){
+  #data.frame method: data should be a data.frame containing the variables referred to in formula, which determines the model matrix
+  terms_data=data.frame(lapply(data[,all.vars(formula),drop=F],function(x)if (is.character(x)) as.factor(x) else x)) #convert character to factor #need drop=F to maintain a dataframe if only one factor!
+  
+  if(sparse){
+    require(Matrix)
+    sparse.model.matrix(formula,terms_data,contrasts.arg = lapply(terms_data[,sapply(terms_data,function(x)is.factor(x)),drop=F], contrasts, contrasts=FALSE)) 
+  }else{
+    model.matrix(formula,terms_data,contrasts.arg = lapply(terms_data[,sapply(terms_data,function(x)is.factor(x)),drop=F], contrasts, contrasts=FALSE)) 
+  }
+  
+}
+modmat_all_levs.data.table=function(formula, data, sparse = F){
+  #data.table method: data should be a data.table containing the variables referred to in formula, which determines the model matrix
+  require(data.table)
+  terms_data=setDT(lapply(data[,all.vars(formula),with=F],function(x)if (is.character(x)) as.factor(x) else x))
+  
+  if(sparse){
+    require(Matrix)
+    sparse.model.matrix(formula,terms_data,contrasts.arg = lapply(terms_data[,sapply(terms_data,function(x)is.factor(x)),with=F,drop=F], contrasts, contrasts=FALSE)) #need drop=F to maintain a dataframe if only one factor!
+  }else{
+    model.matrix(formula,terms_data,contrasts.arg = lapply(terms_data[,sapply(terms_data,function(x)is.factor(x)),with=F,drop=F], contrasts, contrasts=FALSE)) #need drop=F to maintain a dataframe if only one factor!
+  }
+}
+
+
+#function to recode Pew data
+doPewRecode = function(data){
+  
+  is_march_data = ('ftcalldt' %in% names(data))
+  
+  # mode type
+  sample_col = ifelse(is_march_data, 'samptype', 'sample')
+  data[, demo_mode := ifelse(grepl('Cell', get(sample_col)), 'cell', 'landline')]
+  
+  # Date and month called
+  if(is_march_data){
+    data[, date_called := as.Date(as.numeric(substr(ftcalldt,1,6)) - min(as.numeric(substr(data_march18$ftcalldt,1,6))), origin = as.Date('2018-03-07'))]
+    data[, month_called := month(date_called)]
+  }else{
+    data[, date_called := as.Date.character(int_date, format = '%y%m%d')]
+    data[, month_called := month(as.Date.character(int_date, format = '%y%m%d'))]
+  }
+  
+  # age
+  data[, age_num := as.numeric(as.character(age))]
+  
+  data[, demo_age_bucket := cases('99-DNK/refused' = is.na(age_num)
+                                  , '01-Under 30' = age_num < 30
+                                  , '02-30to39' = age_num >= 30 & age_num < 40
+                                  , '03-40to49' = age_num >= 40 & age_num < 50
+                                  , '04-50to59' = age_num >= 50 & age_num < 60
+                                  , '05-60to69' = age_num >= 60 & age_num < 70
+                                  , '06-Over 70' = age_num >= 70
+                                  , '99-DNK/refused' = TRUE 
+                                  )]
+  data[is.na(age_num), demo_age_bucket := '99-DNK/refused']
+  
+  # Sex
+  data[, demo_sex := ifelse(sex == 'Female', '01-female', '02-male')]
+  data[, .N, .(sex, demo_sex)]
+  
+  # Education
+  data[, demo_education := cases('01-postgrad' = grepl("Postgraduate|Some postgraduate", educ)
+                                 , '02-bach' = grepl("Four year", educ)
+                                 , '03-assoc' = grepl("Two year associate|Some college", educ)
+                                 , '04-HS' = grepl("High school graduate", educ)
+                                 , '05-less than HS' = grepl("Less than high school|High school incomplete", educ)
+                                 , '99-DK/refused' = TRUE
+  )]
+  data[, .N, .(educ, demo_education)]
+  
+  # Race/ethnicity
+  data[, demo_hispanic := ifelse(hisp == 'Yes', '01-Y', '02-N')]
+  data[, demo_race := cases('W' = (racecmb == 'White')
+                            , 'B' = (racecmb == 'Black or African-American')
+                            , 'A' = (racecmb == 'Asian or Asian-American')
+                            , 'O' = TRUE
+  )]
+  data[, .N, .(demo_race, racecmb)]
+  
+  
+  # religion
+  data[, demo_relig := cases('01-protestant' = grepl("Protestant|Christian", relig)
+                             , '02-catholic' = grepl("Catholic", relig)
+                             , '03-athiest' = grepl("Athiest|Agnostic", relig)
+                             , '04-jewish' = grepl("Jewish", relig)
+                             , '05-muslim' = grepl("Muslim", relig)
+                             , '06-LDS' = grepl("Mormon", relig)
+                             , '07-other' = TRUE
+  )]
+  data[, .N, .(relig, demo_relig)]
+  
+  ## income
+  data[, demo_income := cases('01-under10k' = (income == 'Less than $10,000')
+                              , '02-10to20k' = (income == '10 to under $20,000')
+                              , '03-20to30k' = (income == '20 to under $30,000')
+                              , '04-30to40k' = (income == '30 to under $40,000')
+                              , '05-40to50k' = (income == '40 to under $50,000')
+                              , '06-50to75k' = (income == '50 to under $75,000')
+                              , '07-75to100k' = (income == '75 to under $100,000')
+                              , '08-100to150k' = (income == '100 to under $150,000 [OR]')
+                              , '09-over150k' = (income == '$150,000 or more')
+                              , '99-DK/refused' = TRUE
+  )]
+  data[, .N, .(demo_income, income)]
+  
+  
+  ## household size
+  data[, demo_hhsize := hh1]
+  
+  ## phone type
+  if(is_march_data){
+    data[, demo_phonetype := cases(
+      '01-Both' = ((samptype == 'Cell Phone' & LL == 'Landline') | (samptype == 'Landline' & l1 == 'Yes, have cell phone'))
+      , '02-LL only' = (samptype == 'Landline' & l1 != 'Yes, have cell phone')
+      , '03-Cell only' = (samptype == 'Cell Phone' & LL == 'No landline')
+      , '99-DNK' = TRUE
+    )]
+    data[, .N, .(demo_phonetype,samptype, l1, l1a, c1, LL)][order(demo_phonetype)]
+  }else{
+    data[, demo_phonetype := cases(
+      '01-Both' = ((sample == 'Cell phone' & ll == 'Landline') | (sample == 'Landline' & ql1 == 'Yes, have cell phone'))
+      , '02-LL only' = (sample == 'Landline' & ql1 != 'Yes, have cell phone')
+      , '03-Cell only' = (sample == 'Cell phone' & ll == 'No landline')
+      , '99-DNK' = TRUE
+    )]
+    data[, .N, .(demo_phonetype, sample, ql1, ql1a, qc1, ll)]
+  }
+  
+  
+  ## state
+  data[, demo_state := as.character(sstate)]
+  
+  ## region
+  data[, demo_region := as.character(scregion)]
+  
+  ## county population density quintile
+  data[, demo_pop_density := cases(
+    '01-Lowest' = sdensity == 'Lowest'
+    , '02' = sdensity == 2
+    , '03' = sdensity == 3
+    , '04' = sdensity == 4
+    , '05-Highest' = sdensity == 'Highest'
+  )]
+  data[, .N, .(sdensity, demo_pop_density)]
+  
+  ## Registered
+  data[, demo_reg := cases(
+    '01-Yes' = grepl('ABSOLUTELY CERTAIN', reg)
+    , '02-Probably' = grepl('PROBABLY', reg)
+    , '03-No' = grepl('NOT', reg)
+    ,'99-DNK/refused' = TRUE
+    )]
+  data[, .N, reg]
+  
+  
+  # Party
+  data[, party := as.character(party)]
+  data[, partyln := as.character(partyln)]
+  data[is.na(partyln), partyln := 'none']
+  data[, demo_party := cases(
+    '01-Dem' = (party == 'Democrat')
+    , '02-Rep' = (party == 'Republican')
+    , '03-Lean Dem' = (partyln == 'Democrat')
+    , '04-Lean Rep' = (partyln == 'Republican')
+    , '05-Ind' = (party == 'Independent' | party == '(VOL) Other party')  & partyln == "(VOL) Other/Don't know/Refused"
+    , '06-None' = party == '(VOL) No preference' & partyln == "(VOL) Other/Don't know/Refused"
+    , '99-DK/refused' = TRUE
+  )]
+  
+  # Ideology
+  data[, demo_ideology := cases(
+    '01-Very liberal' = ideo == 'Very liberal'
+    , '02-Liberal' = ideo == 'Liberal [OR]'
+    , '03-Moderate' = ideo == 'Moderate'
+    , '04-Conservative' = ideo == 'Conservative'
+    , '05-Very conservative' = ideo == 'Very conservative'
+    , '99-DK/refused' = TRUE
+  )]
+  
+  
+  ## Support
+  strong_col = ifelse(max(data$month_called) == 9, 'q7', ifelse(max(data$month_called) == 6, 'q20', 'q8'))
+  lean_col = ifelse(max(data$month_called) == 9, 'q8', ifelse(max(data$month_called) == 6, 'q21', 'q9'))
+  
+  data[, qsupport := NULL]
+  data[get(strong_col) == "Democratic Party's candidate" | get(lean_col) == "Democratic Party's candidate", qsupport := '1-D']
+  data[get(strong_col) == "Republican Party's candidate" | get(lean_col) == "Republican Party's candidate", qsupport := '2-R']
+  data[get(lean_col) == "(VOL) Other", qsupport := '3-O']
+  data[is.na(qsupport), qsupport := '4-DK/R']
+  
+  data[, .N, .(qsupport, get(strong_col), get(lean_col))]
+  
+  data[, y_dem := as.numeric(qsupport == '1-D')]
+  
+  X = data[, grepl('demo_|month_called|y_dem|age', names(data)), with = F]
+  return(X)
+}
+
+# data_recoded[, demo_age_bucket := NULL]
+# data_recoded[, demo_age_bucket := as.character(cases(
+#                                 '01-Under 30' = (age_num < 30)
+#                                 , '02-30to39' = (age_num >= 30 & age_num < 40)
+#                                 , '03-40to49' = (age_num >= 40 & age_num < 50)
+#                                 , '04-50to59' = (age_num >= 50 & age_num < 60)
+#                                 , '05-60to69' = (age_num >= 60 & age_num < 70)
+#                                 , '06-Over 70' = (age_num >= 70)
+#                                 , '99-DNK/refused' = TRUE
+#                                 ))]
+# data_recoded[, class(demo_age_bucket)]
+# 
+# data_recoded[, .N, demo_age_bucket]
+
+
+
+
+getTestTrain = function(data, n_holdout, n_surveyed, n_matched, p_surveyed = NULL, p_matched = NULL){
+  if(is.null(p_surveyed)){
+    p_surveyed = rep(1/nrow(data), n = nrow(data))
+  }
+  if(is.null(p_matched)){
+    p_surveyed = rep(1/n_surveyed, n = n_surveyed)
+  }
+  
+  # holdout set
+  holdout_ind = sample.int(n = nrow(data), size = n_holdout, replace = F)
+  
+  data[, holdout := NULL]
+  data[holdout_ind, holdout := 1]
+  data[-holdout_ind, holdout := 0]
+  
+  # select indicies of those surveyed OF THOSE NOT HELDOUT
+  survey_ind = sample(x = setdiff(1:nrow(data), holdout_ind), size = n_surveyed, replace = F, prob = p_surveyed[-holdout_ind])
+  
+  # make indicators
+  data[, surveyed := NULL]
+  data[survey_ind, surveyed := 1]
+  data[-survey_ind, surveyed := 0]
+  
+  # select matched
+  matched_ind = sample(survey_ind, size = n_matched, replace = F, prob = p_matched[survey_ind])
+  
+  data[, matched := NULL]
+  if(length(matched_ind) > 0){
+    data[matched_ind, matched := 1]
+    data[-matched_ind, matched := 0]
+  }else{
+    data[, matched := 1]
+  }
+  
+  data[, voterfile := ifelse(holdout == 0 & surveyed == 0, 1, 0)]
+  
+  return(list(data = data, holdout_ind = holdout_ind, survey_ind = survey_ind, matched_ind = matched_ind))
+}
+
+
+getBags = function(data, vars, n_bags, newdata = NULL, bags = NULL){
+  # get modmats
+  bags_modmat_fmla = as.formula(paste('~', paste(file_and_survey_vars, collapse = '+')))
+  X_bags = modmat_all_levs(data = data, formula = bags_modmat_fmla)
+  
+  # make bags
+  if(is.null(bags)){
+    bags = kmeans(x = X_bags, centers = n_bags)
+  }
+  
+  if(!is.null(newdata)){
+    X_bags_file = modmat_all_levs(data = newdata, formula = bags_modmat_fmla)
+    bags_newdata = predict.kmeans(object = bags, newdata = X_bags_file, method = 'classes')
+  }else{
+    bags_newdata = NULL
+  }
+  
+  return(list(bags = bags$cluster, bags_newdata = bags_newdata))
+}
+
+
+getLandmarks = function(data, vars, n_landmarks, subset_ind = NULL){
+  modmat_fmla = as.formula(paste('~', paste(vars, collapse = '+')))
+  
+  # make one modmat so we get all columns in all subsets
+  X = modmat_all_levs(data = data, formula = modmat_fmla)
+  
+  # get group definitions with k-means
+  landmarks = kmeans(x = X[subset_ind, ], centers = n_landmarks)
+  landmarks = as.matrix(landmarks$centers)
+  
+  return(list(landmarks = landmarks, X = X))
+}
+
+
+
+# https://stats.stackexchange.com/questions/12623/predicting-cluster-of-a-new-object-with-kmeans-in-r/188628
+predict.kmeans <- function(object,
+                           newdata,
+                           method = c("centers", "classes")) {
+  method <- match.arg(method)
+  
+  centers <- object$centers
+  ss_by_center <- apply(centers, 1, function(x) {
+    colSums((t(newdata) - x) ^ 2)
+  })
+  best_clusters <- apply(ss_by_center, 1, which.min)
+  
+  if (method == "centers") {
+    centers[best_clusters, ]
+  } else {
+    best_clusters
+  }
+}
+
+
+getFeatures = function(train, train_bag, test, test_bag, landmarks, sigma){
+  # create kernel
+  rbf = rbfdot(sigma = sigma)
+  
+  # calculate features for train
+  phi_x_train = kernelMatrix(rbf, x = as.matrix(train), y = landmarks)
+  phi_x_train = data.table(bag = train_bag, phi_x_train)
+  setnames(phi_x_train, c('bag', paste0('u', 1:nrow(landmarks))))
+  
+  # calculate for test
+  phi_x_test = kernelMatrix(rbf, x = as.matrix(test), y = landmarks)
+  phi_x_test = data.table(bag = test_bag, phi_x_test)
+  setnames(phi_x_test, c('bag', paste0('u', 1:nrow(landmarks))))
+  
+  # calculate means of embeddings
+  mu_hat_train = phi_x_train[, lapply(.SD, mean), .SDcols = names(phi_x_train)[-1], by = 'bag'][order(bag)]
+  
+  return(list(mu_hat = mu_hat_train, phi_x_train = phi_x_train, phi_x_test = phi_x_test))
+}
+
+fitLasso = function(mu_hat, Y_bag, phi_x_train = NULL, phi_x_test =  NULL, nfolds = 10){
+  
+  # fit once to get non-zero coefs
+  # cat(dim(mu_hat))
+  # cat('\nY_bag:')
+  # cat(length(Y_bag))
+  # cat('\n')
+  fit_lambda = cv.glmnet(x = as.matrix(mu_hat[, -1, with = F]), y = Y_bag, nfolds = nfolds)
+  nonzero_ind = which(coef(fit_lambda, s = 'lambda.min') != 0)
+  
+  # cat(nonzero_ind)
+  # cat('\n')
+  
+  # refit to avoid shrinkage
+  fit = glmnet(as.matrix(mu_hat[, nonzero_ind + 1, with = F]), y = Y_bag, lambda = 0)
+  
+  if(!is.null(phi_x_train)){
+    Y_train = predict(fit, newx = as.matrix(phi_x_train[, nonzero_ind + 1, with = F]))
+  }else{
+    Y_train = NULL
+  }
+  if(!is.null(phi_x_test)){
+    Y_test = predict(fit, newx = as.matrix(phi_x_test[, nonzero_ind + 1, with = F]))
+  }else{
+    Y_test = NULL
+  }
+  
+  
+  return(list(fit = fit, Y_train = Y_train, Y_test = Y_test))
+}
+
+
+
+doBasicDR = function(data
+                     , bagging_vars
+                     , regression_vars
+                     , outcome
+                     , n_bags
+                     , n_landmarks
+                     , sigma
+                     , bagging_ind = 'surveyed'
+                     , train_ind = 'voterfile'
+                     , test_ind = 'holdout'
+){
+  
+  # cat(sum(data[, get(bagging_ind)]))
+  # cat('\n')
+  # cat(sum(data[, get(train_ind)]))
+  # cat('\n')
+  # cat(sum(data[, get(test_ind)]))
+  # cat('\n')
+  
+  # Make bags
+  bags = getBags(data = data[get(bagging_ind) == 1,]
+                 , vars = bagging_vars
+                 , n_bags = n_bags
+                 , newdata = data[get(train_ind) == 1 | get(test_ind) == 1, ])
+  
+  # assign data to bags
+  data[get(bagging_ind) == 1, bag := bags$bags]
+  data[get(train_ind) == 1 | get(test_ind) == 1, bag := bags$bags_newdata]
+  
+  # calculate dependent var in each bag
+  Y_svy_bag = data[get(bagging_ind) == 1, .(y_mean = mean(get(outcome))), bag][order(bag)]
+  
+  # Get landmarks
+  landmarks = getLandmarks(data = data
+                           , vars = regression_vars
+                           , n_landmarks = n_landmarks
+                           , subset_ind = (data[, get(train_ind)] == 1))
+  
+  # Make matricies for feature embedding
+  X_file = landmarks$X[data[, get(train_ind)] == 1, ]
+  X_file_holdout = landmarks$X[data[, get(test_ind)] == 1, ]
+  
+  # get features
+  features = getFeatures(train = X_file
+                         , train_bag = data[get(train_ind) == 1, bag]
+                         , test = X_file_holdout
+                         , test_bag = data[get(test_ind) == 1, bag]
+                         , landmarks = landmarks$landmarks
+                         , sigma = sigma)
+  
+  # do basic DR
+  fit = fitLasso(mu_hat = features$mu_hat
+                 , Y_bag = Y_svy_bag$y_mean
+                 , phi_x_train = features$phi_x_train
+                 , phi_x_test = features$phi_x_test
+  )
+  
+  # score the file
+  data[get(train_ind) == 1, y_hat := fit$Y_train]
+  data[get(test_ind) == 1, y_hat := fit$Y_test]
+  
+  # calculate mse
+  mse_test = calcMSE(Y = as.numeric(data[get(test_ind) == 1, get(outcome)])
+                     , Y_pred = as.numeric(data[get(test_ind) == 1, y_hat]))
+  
+  return(list(data = data, fit = fit$fit, landmarks = landmarks$landmarks, bags = data$bag, y_hat = data$y_hat, mse_test = mse_test))
+}
+
