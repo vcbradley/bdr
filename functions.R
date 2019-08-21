8957674e427ccc84041e1bf486792a28bfa36c0e
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-08-21 13:51:04 +0100

    get rid of ridgeMat function (unnecessary)

--- functions.R
+++ functions.R
@@ -587,6 +587,7 @@ doBasicDR = function(data
   
   
   # prep outcome
+  # if weighting col is specified, then use that to get weighted mean
   if(family == 'multinomial'){
     Y_svy_bag = data[get(bagging_ind) == 1, lapply(.SD, weighted.mean, w = weight), .SDcols = outcome, by = bag][order(bag)]
   }else{
@@ -653,53 +654,58 @@ doDecilePlot = function(data, score_name, title = NULL){
   return(plot)
 }
 
-
-ridgeMat = function(origMat){
-  cholStatus <- try(u <- chol(origMat), silent = TRUE)
-  cholError <- ifelse(class(cholStatus) == "try-error", TRUE, FALSE)
-  
-  newMat <- origMat
-  iter <- 0
-  while (cholError) {
-    
-    iter <- iter + 1
-    cat("iteration ", iter, "\n")
-    
-    # replace -ve eigen values with small +ve number
-    newEig <- eigen(newMat)
-    newEig2 <- newEig$values + 2 *abs(min(newEig$values))# + 1e-10  # add in the min eigenvalue
-    #newEig2 <- ifelse(newEig$values < 1e-10, 1e-10, newEig$values)
-    
-    # create modified matrix eqn 5 from Brissette et al 2007, inv = transp for
-    # eig vectors
-    newMat <- newEig$vectors %*% diag(newEig2) %*% t(newEig$vectors)
-    
-    # normalize modified matrix eqn 6 from Brissette et al 2007
-    newMat <- newMat/sqrt(diag(newMat) %*% t(diag(newMat)))
-    
-    # try chol again
-    cholStatus <- try(u <- chol(newMat), silent = TRUE)
-    cholError <- ifelse(class(cholStatus) == "try-error", TRUE, FALSE)
-  }
-  return(newMat)
-}
+### replacing with nearPD from the Matrix package
+# ridgeMat = function(origMat){
+#   cholStatus <- try(u <- chol(origMat), silent = TRUE)
+#   cholError <- ifelse(class(cholStatus) == "try-error", TRUE, FALSE)
+#   
+#   newMat <- origMat
+#   iter <- 0
+#   while (cholError) {
+#     
+#     iter <- iter + 1
+#     cat("iteration ", iter, "\n")
+#     
+#     # replace -ve eigen values with small +ve number
+#     newEig <- eigen(newMat)
+#     newEig2 <- newEig$values + 2 *abs(min(newEig$values))# + 1e-10  # add in the min eigenvalue
+#     #newEig2 <- ifelse(newEig$values < 1e-10, 1e-10, newEig$values)
+#     
+#     # create modified matrix eqn 5 from Brissette et al 2007, inv = transp for
+#     # eig vectors
+#     newMat <- newEig$vectors %*% diag(newEig2) %*% t(newEig$vectors)
+#     
+#     # normalize modified matrix eqn 6 from Brissette et al 2007
+#     newMat <- newMat/sqrt(diag(newMat) %*% t(diag(newMat)))
+#     
+#     # try chol again
+#     cholStatus <- try(u <- chol(newMat), silent = TRUE)
+#     cholError <- ifelse(class(cholStatus) == "try-error", TRUE, FALSE)
+#   }
+#   return(newMat)
+# }
 
 
 doKMM = function(X_trn, X_tst, B = 1, sigma = 1, kernel_type = 'rbf'){
+  require(Matrix)
+  
   n_trn = nrow(X_trn)
   n_tst = nrow(X_tst)
   
   eps = B/sqrt(n_trn)  # set epsilon based on B and suggested value from Gretton chapter; this constraint ensures that  Beta * the training dist is close to a probability dist
   
   # use RBF kernel for now
-  rbf1 = rbfdot(sigma = sigma)
-  K = kernelMatrix(rbf1, x = X_trn)
+  #rbf1 = rbfdot(sigma = sigma)
+  #K = kernelMatrix(rbf1, x = X_trn)
+  
+  kern = vanilladot()
+  K = kernelMatrix(kern, x = X_trn)
   
   # fix to make sure we can use Cholesky decomp
   newK = nearPD(K)$mat
-  chol(newK)
+  #chol(newK)
   
-  kappa = kernelMatrix(rbf1, x = X_trn, y = X_tst)
+  kappa = kernelMatrix(kern, x = X_trn, y = X_tst)
   kappa = (n_trn/n_tst) * rowSums(kappa)
   
   G = as.matrix(rbind(- rep(1, n_trn)
@@ -720,10 +726,15 @@ doKMM = function(X_trn, X_tst, B = 1, sigma = 1, kernel_type = 'rbf'){
 
 
 
-getWeights = function(data, vars, train_ind, target_ind, B = 5, sigma = 0.1){
+getWeights = function(data, vars, train_ind, target_ind, weight_col = NULL, B = 5, sigma = 0.1){
   
-  fmla = as.formula(paste0('~', paste(vars, collapse = '+')))
+  fmla = as.formula(paste0('~-1', paste(vars, collapse = '+')))
   X = model.matrix(object = fmla, data = data)  #not all levels
+  
+  # if weights are supplied, multiply them through the observations of X
+  if(!is.null(weight_col)){
+    X = diag(data[, get(weight_col)]) %*% X
+  }
   
   X_train = X[which(data[, get(train_ind)] == 1),]    # data to weight
   X_target = X[which(data[, get(target_ind)] == 1),]  # target
