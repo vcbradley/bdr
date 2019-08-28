c1f6cdb2f21fb51fa50a39acdbf66d2650f3c069
vcbradley <valerie.bradley@new.ox.ac.uk>
2019-08-28 13:21:00 +0100

    drop nearPD - don't need this to always be true

--- utils/functions_customkern.R
+++ utils/functions_customkern.R
@@ -14,21 +14,26 @@ getCustomKern = function(X, Y = NULL, kernel_params){
   
   if(!is.null(kernel_params$linear_ind)){
     kern_lin = vanilladot()
-    K_lin = kernelMatrix(kern_lin, matrix(X[, kernel_params$linear_ind]), matrix(Y[, kernel_params$linear_ind]))
+    K_lin = kernelMatrix(kern_lin
+                         , as.matrix(X[, kernel_params$linear_ind])
+                         , as.matrix(Y[, kernel_params$linear_ind]))
   }else{
     K_lin = matrix(0, k_rows, k_cols)
   }
   
   if(!is.null(kernel_params$rbf_ind)){
     kern_rbf = rbfdot(sigma = kernel_params$sigma)
-    K_rbf = kernelMatrix(kern_lin, matrix(X[, kernel_params$rbf_ind]), matrix(Y[, kernel_params$rbf_ind]))
+    K_rbf = kernelMatrix(kern_rbf
+                         , as.matrix(X[, kernel_params$rbf_ind])
+                         , as.matrix(Y[, kernel_params$rbf_ind])
+                         )
   }else{
     K_rbf = matrix(0, k_rows, k_cols)
   }
   
   if(!is.null(kernel_params$matern_ind)){
-    K_matern = matern.cov(matrix(X[, kernel_params$matern_ind])
-                          , matrix(Y[, kernel_params$matern_ind])
+    K_matern = matern.cov(as.matrix(X[, kernel_params$matern_ind])
+                          , as.matrix(Y[, kernel_params$matern_ind])
                           , theta = kernel_params$theta
                           , smoothness = kernel_params$smoothness
                           , scale = kernel_params$scale
@@ -38,9 +43,6 @@ getCustomKern = function(X, Y = NULL, kernel_params){
   }
   
   K_full = K_lin + K_rbf + K_matern
-  
-  # this takes a while ugh
-  K_new = nearPD(K_full)$mat
   
   return(K_new)
 }
