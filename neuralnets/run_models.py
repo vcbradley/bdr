f18c5439b9b11a1c61c06de10bed739950e724c4
vcbradley <valerie.bradley@new.ox.ac.uk>
2020-01-17 16:02:05 +0000

    features working

--- neuralnets/run_models.py
+++ neuralnets/run_models.py
@@ -6,10 +6,12 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import Ridge
+from sklearn.metrics.pairwise import rbf_kernel
 import tensorflow as tf
 
 from neuralnets.features import Features
-from neuralnets import radial
+import neuralnets.radial
+from neuralnets.radial import build_radial_net
 from neuralnets.train import eval_network, train_network
 from neuralnets.utils import get_median_sqdist, tf_session
 
@@ -64,29 +66,47 @@ landmark_ind = turnout_ldmks[0].astype(int)
 
 
 
+# X_latlong_grouped = X_latlong.groupby('code_num')
+# X_latlong_np = []
+# for name, group in X_latlong_grouped:
+#     X_latlong_np.append(group.values[:,2:])
+# X_latlong_np
 
 
+#### MAKE FEATURES
+bags = X_latlong.to_numpy()[:,2:]
+n_pts = X_latlong.groupby('code_num')['code_num'].count().to_numpy()
 
-INPUTS
+feats = Features(bags = bags, n_pts=n_pts, stack=True, copy=False, bare=False, y = outcome_data['pct_turnout_ge2017'])
 
-def make_network(args, train):_
-    kw = dict(
-            in_dim=train.dim,
-            reg_out=args.reg_out,
-            reg_out_bias=args.reg_out_bias,
-            scale_reg_by_n=args.scale_reg_by_n,
-            dtype=tf.float64 if args.dtype_double else tf.float32,
-        )
 
-    kw['bw'] = bw = np.sqrt(get_median_sqdist(train) / 2) * args.bw_scale
-    need_means = args.init_from_ridge
+args = {'reg_out':0
+        , 'reg_out_bias':0
+        , 'scale_reg_by_n':False
+        , 'dtype_double':False
+        , 'type': 'radial'
+        , 'init_from_ridge':False
+        , 'landmarks':None
+        }
+
+
+def make_network(args, train):
+    kw = {'in_dim': train.dim
+        , 'reg_out': args['reg_out']
+        , 'reg_out_bias': args['reg_out_bias']
+        , 'scale_reg_by_n': args['scale_reg_by_n']
+        , 'dtype': tf.float64 if args['dtype_double'] else tf.float32
+          }
+
+    kw['bw'] = bw = np.sqrt(get_median_sqdist(train) / 2) #* args['bw_scale']
+    need_means = args['init_from_ridge']
     need_all_feats = False
 
     # get landmarks
-    kw['landmarks'] = landmarks = pick_landmarks(args, train)
-    kw['opt_landmarks'] = args.opt_landmarks
+    #kw['landmarks'] = landmarks = pick_landmarks(args, train)
+    kw['landmarks'] = landmarks = args['landmarks']
 
-    get_f = partial(rbf_kernel, Y=landmarks, gamma=1 / (2 * bw**2))
+    get_f = partial(rbf_kernel, Y=landmarks, gamma=1 / (2 * bw**2))  #creating a partial function call of rbf_kernel with Y and gamma fixed
     if need_all_feats:
         train.make_stacked()
         train_feats = Features(get_f(train.stacked_features), train.n_pts)
@@ -98,9 +118,9 @@ def make_network(args, train):_
             train_means[i] = get_f(bag).mean(axis=0)
 
     # initialize weights with ridge regression
-    if args.init_from_ridge:
+    if args['init_from_ridge']:
         print("Fitting ridge...")
-        ridge = Ridge(alpha=2 * args.reg_out * len(train),
+        ridge = Ridge(alpha=2 * args['reg_out'] * len(train),
                       solver='saga', tol=0.1, max_iter=500)
         # Ridge mins  ||y - X w - b||^2 + alpha ||w||^2
         # we min  1/n ||y - X w - b||^2 + reg_out/2 ||w||^2
@@ -111,7 +131,8 @@ def make_network(args, train):_
         kw['init_out'] = ridge.coef_
         kw['init_out_bias'] = ridge.intercept_
 
-    kw['init_R_scale'] = args.init_R_scale
-    kw['opt_R_scale'] = args.opt_R_scale
+    return build_radial_net(**kw)
 
-    return network_types[args.type](**kw)
+
+make_network(args, feats)
+
